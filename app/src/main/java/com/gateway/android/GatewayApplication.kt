54deba231a227f54c685dcbb5befaebaeec04b4f
aydozkan <aydinmehmet.ozkan@ingbank.com.tr>
2019-01-09 22:40:35 +0300

    Add - FirebaseRemoteConfig

--- app/src/main/java/com/gateway/android/GatewayApplication.kt
+++ app/src/main/java/com/gateway/android/GatewayApplication.kt
@@ -1,12 +1,12 @@
 package com.gateway.android
 
 import android.app.Application
-import com.gateway.android.sms.SmsBroadcastReceiver
+import com.gateway.android.utils.SharedPreferencesWrapper
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 
 class GatewayApplication : Application() {
-
-    private var smsBroadcastReceiver: SmsBroadcastReceiver? = null
 
     override fun onCreate() {
         super.onCreate()
@@ -15,6 +15,22 @@ class GatewayApplication : Application() {
             FirebaseMessaging.getInstance().subscribeToTopic("gateway-debug")
         } else {
             FirebaseMessaging.getInstance().subscribeToTopic("gateway")
+        }
+
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setDeveloperModeEnabled(BuildConfig.DEBUG)
+            .build()
+
+        val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+
+        firebaseRemoteConfig.setConfigSettings(configSettings)
+
+        firebaseRemoteConfig.fetch(43200).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                firebaseRemoteConfig.activateFetched()
+
+                SharedPreferencesWrapper(this).baseUrl = firebaseRemoteConfig.getString("base_url")
+            }
         }
     }
 }
